f6d637d0d6964ce38969d9c8a0d8114a06204803
jubianchi <julien.bianchi@pmsipilot.com>
2014-09-21 00:32:40 +0200

    Use YAML configuration files

--- DependencyInjection/HoathisConsoleExtension.php
+++ DependencyInjection/HoathisConsoleExtension.php
@@ -3,7 +3,7 @@
 namespace Hoathis\Bundle\ConsoleBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
 
@@ -11,9 +11,9 @@ class HoathisConsoleExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('console.xml');
-        $loader->load('helpers.xml');
-        $loader->load('styles.xml');
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services/console.yml');
+        //$loader->load('services/helpers.yml');
+        //$loader->load('services/styles.yml');
     }
 }
